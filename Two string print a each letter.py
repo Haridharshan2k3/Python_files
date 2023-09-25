152e66300ddc9f1040abae4217dbf6784ebab398
Haridharshan2k3 <145430642+Haridharshan2k3@users.noreply.github.com>
2023-09-25 18:37:44 +0530

    my_python_code

--- Two string print a each letter.py
+++ Two string print a each letter.py
@@ -6,8 +6,8 @@ while i<len(s1) or j<len(s2):
     if i<len(s1):
         output=output+s1[i]
         i+=1
-        if j<len(s2):
-            output=output+s2[j]
-            j=j+1
-print(output,end="")
+    if j<len(s2):
+        output=output+s2[j]
+        j+=1
+print(output)
 # end="" used to print the output in a single or same line
